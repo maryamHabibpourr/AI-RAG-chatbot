25c102e49d447883eed575860be061f840a66f63
maryamHabibpourr <Einesheikh_12@yahoo.com>
2025-02-18 20:45:01 -0800

    implemented chat interface and api route

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,50 @@
+import { streamText } from "ai";
+import { createOpenAI } from "@ai-sdk/openai";
+
+export const runtime = "edge";
+
+const openai = createOpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY || "",
+  compatibility: "strict",
+});
+
+
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const stream = await streamText({
+    model: openai("gpt-4o-mini"),
+    messages: messages,
+    temperature: 1,
+  });
+  return stream?.toDataStreamResponse();
+}
+
+
+
+
+
+//old versian
+// import OpenAI from 'openai';
+// import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+// export const runtime = 'edge';
+
+// const openai = new OpenAI({
+//     apiKey: process.env.OPENAI_API_KEY!,
+// });
+
+// export async function POST(req: Request) {
+//     const { messages } = await req.json();
+
+//     const response = await openai.chat.completions.create({
+//         model: 'gpt-3.5-turbo',
+//         stream: true,
+//         messages: messages,
+//     });
+
+//     const stream = OpenAIStream(response);
+
+//     return new StreamingTextResponse(stream);
+// }
